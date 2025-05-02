43d0b3aa6ccc2527e1e94c0d7a9339643c41d40f
nikitosalo <nssalikov@gmail.com>
2025-05-02 06:25:38 +0300

    feat: add build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import { resolve } from "path";
+import { resolve } from "node:path";
+import * as packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,8 +12,15 @@ export default defineConfig({
       tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
     }),
   ],
+  define: {
+    // "process.env": process.env,
+    // // By default, Vite doesn't include shims for NodeJS/
+    // // necessary for segment analytics lib to work
+    "global": {},
+  },
   build: {
     copyPublicDir: false,
+    sourcemap: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "lib/index.ts"),
@@ -20,5 +28,13 @@ export default defineConfig({
       fileName: "index",
       formats: ["es", "umd"],
     },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+      output: {
+        globals: {
+          'mobx': 'mobx',
+        }
+      }
+    }
   },
 });
